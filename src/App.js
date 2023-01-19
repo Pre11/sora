268d7281aead5baa6309e80128db762c9259ed47
Pre11 <preetichoudhury2001@gmail.com>
2023-01-19 12:34:36 +0530

    sora

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Dashboard from './pages/Dashboard';
+// import { BrowserRouter} from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+     
+    
+      <Dashboard />
+      
+    
+    
+    </div>
+  );
+}
+
+export default App;
